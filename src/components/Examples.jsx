239fabcba9added36a2cfb7e2cf796ca002764ed
M.D.W.De Silva <wdulanjith@gmail.com>
2024-10-08 11:46:54 +0530

    Setting default prop values instead of parsing it

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -30,7 +30,6 @@ export default function Examples() {
         <>
             <Section title="Examples" id="examples">
                 <Tabs
-                    ButtonsContainer="menu"
                     buttons={
                         <>
                             <TabButton
